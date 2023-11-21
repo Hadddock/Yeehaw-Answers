16375e99a37df2594b83f6eead9a4ee4f859caf6
Justin Haddock <jhadddock@protonmail.com>
2023-11-21 09:02:29 -0800

    Add Post model

---
+++ models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true, minLength: 5, maxLength: 200 },
+  body: { type: String, minlength: 3, maxLength: 30000 },
+  date_posted: { type: Date },
+  date_last_edited: { type: Date },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
