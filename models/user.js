5558a357684a61af6ba382972f1db63baedc521b
Haddock <jhadddock@protonmail.com>
2023-11-20 16:34:30 -0800

    Add User model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  date_of_birth: { type: Date },
+  date_joined: { type: Date },
+  username: { type: String, required: true, maxLength: 100, minLength: 1 },
+});
+
+UserSchema.virtual("time_active").get(function () {
+  return new Date() - this.date_of_birth;
+});
+
+module.exports = mongoose.model("User", UserSchema);
