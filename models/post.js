a950211d8e7613c55171485b97f222ce8eb97d06
Justin Haddock <jhadddock@protonmail.com>
2023-11-30 10:52:28 -0800

    Add post creation form

--- models/post.js
+++ models/post.js
@@ -2,40 +2,43 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const { DateTime } = require("luxon");
+const categories = [
+  "Arts & Humanities",
+  "Beauty & Style",
+  "Business & Finance",
+  "Cars & Transportation",
+  "Computers & Internet",
+  "Consumer Electronics",
+  "Dining Out",
+  "Education & Reference",
+  "Entertainment & Music",
+  "Environment",
+  "Family & Relationships",
+  "Food & Drink",
+  "Games & Recreation",
+  "Health",
+  "Home & Garden",
+  "Local Businesses",
+  "News & Events",
+  "Pets",
+  "Politics & Government",
+  "Pregnancy & Parenting",
+  "Science & Mathematics",
+  "Social Science",
+  "Society & Culture",
+  "Sports",
+  "Travel",
+  "Yahoo Products",
+];
+
+exports.categories = categories;
 
 const PostSchema = new Schema({
   title: { type: String, required: true, minLength: 5, maxLength: 200 },
-  body: { type: String, minlength: 3, maxLength: 30000 },
+  body: { type: String, maxLength: 30000 },
   category: {
     type: String,
-    enum: [
-      "Arts & Humanities",
-      "Beauty & Style",
-      "Business & Finance",
-      "Cars & Transportation",
-      "Computers & Internet",
-      "Consumer Electronics",
-      "Dining Out",
-      "Education & Reference",
-      "Entertainment & Music",
-      "Environment",
-      "Family & Relationships",
-      "Food & Drink",
-      "Games & Recreation",
-      "Health",
-      "Home & Garden",
-      "Local Businesses",
-      "News & Events",
-      "Pets",
-      "Politics & Government",
-      "Pregnancy & Parenting",
-      "Science & Mathematics",
-      "Social Science",
-      "Society & Culture",
-      "Sports",
-      "Travel",
-      "Yahoo Products",
-    ],
+    enum: categories,
   },
   date_posted: { type: Date },
   date_last_edited: { type: Date },
@@ -57,4 +60,4 @@ PostSchema.virtual("url").get(function () {
   return `/catalog/post/${this._id}`;
 });
 
-module.exports = mongoose.model("Post", PostSchema);
+exports.Post = mongoose.model("Post", PostSchema);
